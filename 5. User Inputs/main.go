76c1cf873e6ff83ce13156306b297c74559e5f26
avayyyyyyy <jabhi465@gmail.com>
2024-06-20 18:20:17 +0530

    feat: ⚡️ Learned How to take inputs with scan and bufio

--- 5. User Inputs/main.go
+++ 5. User Inputs/main.go
@@ -9,7 +9,14 @@ import (
 
 func main(){
 
+	var name string;
+
 	fmt.Println("Tell me your name!")
+	
+	fmt.Scan(&name)
+	
+	fmt.Printf("Hii, %s",name)
+	
 	var reader = bufio.NewReader(os.Stdin)
 
 	var name, _  = reader.ReadString('\n')
